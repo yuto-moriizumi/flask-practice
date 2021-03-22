65e296ad1a4c9ec4a28e15768f9e8589209baa87
Yuto Moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2021-03-22 16:58:38 +0900

    CSSを導入

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -5,4 +5,6 @@ app = Flask(__name__)
 app.config.from_object('flask_blog.config')
 db = SQLAlchemy(app)
 
+# 注意：from flask_blog.views import views,entries は最後に書くこと
+# VSCodeのフォーマッタが上に押し上げてしまいますが、そうするとバグります！！！
 from flask_blog.views import views,entries
